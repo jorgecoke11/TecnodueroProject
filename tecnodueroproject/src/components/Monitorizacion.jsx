41631113186cbc9530404747774aedceba64feb2
Jorge López <lopezcarneroj@gmail.com>
2024-08-24 17:35:23 +0200

    Procesos sin hardcodear

--- tecnodueroproject/src/components/Monitorizacion.jsx
+++ tecnodueroproject/src/components/Monitorizacion.jsx
@@ -1,28 +1,45 @@
 import React, { useState, useEffect } from "react";
 import MonitorizacionProceso from "./MonitorizacionProceso"
 import DropDownComponent from './DropDown';
+import procesosServices from '../services/Procesos.js'
 import atras from '../img/hacia-atras.png'
-const Monitorizacion = () =>{
-    const [screenComponent, setScreenComponent] = useState(null)
-    const optionsRobots =['Extraccion de precios BSH']
-    const handleSelectChange = (selectOption) =>{
-        switch(selectOption){
-          case 'Extraccion de precios BSH':
-            setScreenComponent(<MonitorizacionProceso nombreProceso={"Extraccion de precios BSH"} id_proceso={1}></MonitorizacionProceso>)
-            break
-          default:
-            setScreenComponent('')
-        }
+import useUser from "../hooks/useUser.js";
+const Monitorizacion = () => {
+  const { jwt } = useUser();
+  const [screenComponent, setScreenComponent] = useState(null)
+  const optionsRobots = ['Extraccion de precios BSH']
+  const [procesos, setProcesos] = useState([])
+  const handleSelectChange = (selectOption) => {
+    console.log(selectOption)
+    let procesoElegido = procesos.find(proceso => proceso.Nombre === selectOption)
+    setScreenComponent(<MonitorizacionProceso nombreProceso={procesoElegido.Nombre} id_proceso={procesoElegido.idRobot}></MonitorizacionProceso>)
+
+  }
+  const handleAtras = () => {
+    window.location = '/'
+  }
+  useEffect(() => {
+    GetProcesos()
+  }, []);
+  const GetProcesos = async () => {
+    try {
+      const procesos = await procesosServices.getProcesos(jwt)
+      setProcesos(procesos)
+
+    } catch (error) {
+      if (error.response.status === 510) {
+        window.sessionStorage.clear();
+        window.location = "/";
       }
-      const handleAtras = () =>{
-        window.location= '/'
-      }
-    return(
+    }
+  }
+    return (
       <div class="mx-auto">
-          <img src={atras} className='atras' onClick={handleAtras} alt='atras'></img>
-            <DropDownComponent label={'Monitorizacion de Procesos'} options={optionsRobots} onSelectChange={handleSelectChange}/>
-            {screenComponent}
+        <img src={atras} className='atras' onClick={handleAtras} alt='atras'></img>
+        <DropDownComponent label={'Monitorizacion de Procesos'} options={optionsRobots} onSelectChange={handleSelectChange} />
+        {screenComponent}
       </div>
     )
-}
-export default Monitorizacion
+  }
+
+  export default Monitorizacion
